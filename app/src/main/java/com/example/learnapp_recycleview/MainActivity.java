a9e0622b060b34f49feae45fce01c39fa9433792
rizqul <kikiqullah@gmail.com>
2021-04-29 23:55:05 +0800

    Add bottom navigation view

--- app/src/main/java/com/example/learnapp_recycleview/MainActivity.java
+++ app/src/main/java/com/example/learnapp_recycleview/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.learnapp_recycleview;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.ListAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +30,11 @@ public class MainActivity extends AppCompatActivity implements ListHeroAdapter.O
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = (BottomNavigationView) findViewById(R.id.bn_menu);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+//        getSupportFragmentManager().beginTransaction().replace(R.id.fl_container, new HomeFragment());
+
         recyclerViewHero = findViewById(R.id.rv_hero);
         recyclerViewHero.setHasFixedSize(true);
         models.addAll(HeroDatabase.getListData());
@@ -33,6 +43,29 @@ public class MainActivity extends AppCompatActivity implements ListHeroAdapter.O
         recyclerViewHero.setAdapter(adapter);
         adapter.setOnItemClickListener(MainActivity.this);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.nm_home:
+                    selectedFragment = new HomeFragment();
+                    break;
+
+                case R.id.nm_history:
+                    selectedFragment = new HistoryFragment();
+                    break;
+
+                case R.id.nm_bookmark:
+                    selectedFragment = new BookmarkFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.rv_hero, selectedFragment).commit();
+            return true;
+        }
+    };
 
     @Override
     public void onItemClick(int position) {
